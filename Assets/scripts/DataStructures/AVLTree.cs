eb488ac2f93f49cc62e5840aa15b180275e17d17
GiorgosDiamadis <56475498+GiorgosDiamadis@users.noreply.github.com>
2020-10-09 12:07:09 +0300

    Makind add visual

--- Assets/scripts/DataStructures/AVLTree.cs
+++ Assets/scripts/DataStructures/AVLTree.cs
@@ -18,29 +18,9 @@ public class AVLTree : BinaryTree
         Stack<BinaryTreeNode> parents = new Stack<BinaryTreeNode>();
         BinaryTreeNode new_node = new BinaryTreeNode(data);
         bool exists = false;
-        int pos = 0;
 
-        while (current != null)
-        {
-            if (data < current.Get_Data())
-            {
-                pos = 2 * pos + 1;
-                current = current.Get_Left();
-            }
-            else if (data > current.Get_Data())
-            {
-                pos = 2 * pos + 2;
-                current = current.Get_Right();
-            }
-            else if (data == current.Get_Data())
-            {
-                pos = 0;
-                print(pos);
-                break;
-            }
-        }
+        int pos = Check_Can_Add(data);
 
-        
         if (pos < 31)
         {
             current = head;
@@ -80,63 +60,12 @@ public class AVLTree : BinaryTree
 
                 new_node.Set_Scene_Object(Instantiate(node_prefab, view.transform), data);
 
-                int position = 0;
-                int parent_position = -1;
-                Queue<BinaryTreeNode> queue = new Queue<BinaryTreeNode>();
-                Queue<int> position_queue = new Queue<int>();
-                Queue<int> parent_queue = new Queue<int>();
+                Update_Visual();
 
-                queue.Enqueue(head);
-                position_queue.Enqueue(position);
-                parent_queue.Enqueue(parent_position);
-
-                while (queue.Count != 0)
-                {
-                    current = queue.Dequeue();
-                    position = position_queue.Dequeue();
-                    parent_position = parent_queue.Dequeue();
-
-
-                    current.Get_GameObject().transform.localPosition = positions[position];
-
-                    if (parent_position != -1)
-                    {
-                        if (current.Get_Child_Type() == BinaryTreeNode.Child_Type.Left)
-                        {
-                            current.Get_Parent().Get_GameObject().transform.Get_Child(2).localScale = scales[parent_position];
-                            current.Get_Parent().Get_GameObject().transform.Get_Child(2).eulerAngles = new Vector3(0, 0, rotations_left[parent_position]);
-                        }
-                        else
-                        {
-                            current.Get_Parent().Get_GameObject().transform.Get_Child(3).localScale = scales[parent_position];
-                            current.Get_Parent().Get_GameObject().transform.Get_Child(3).eulerAngles = new Vector3(0, 0, rotations_right[parent_position]);
-                        }
-                    }
-
-                    if (position >= 15)
-                    {
-                        current.Get_GameObject().transform.Set_Child_Active(active: false, 2);
-                        current.Get_GameObject().transform.Set_Child_Active(active: false, 3);
-                    }
-
-                    if (current.Get_Left() != null)
-                    {
-                        queue.Enqueue(current.Get_Left());
-                        parent_queue.Enqueue(position);
-                        position_queue.Enqueue(2 * position + 1);
-                    }
-                    if (current.Get_Right() != null)
-                    {
-                        queue.Enqueue(current.Get_Right());
-                        parent_queue.Enqueue(position);
-                        position_queue.Enqueue(2 * position + 2);
-                    }
-                }
-                
-                
                 new_node.Change_Height();
                 Rebalance(parents);
 
+                Update_Visual();
 
                 yield return null;
             }
@@ -150,7 +79,109 @@ public class AVLTree : BinaryTree
         GameHandler.Instance.is_running = false;
     }
 
+    private int Check_Can_Add(long data)
+    {
+        BinaryTreeNode current = head;
+        int pos = 0;
+        while (current != null)
+        {
+            if (data < current.Get_Data())
+            {
+                pos = 2 * pos + 1;
+                current = current.Get_Left();
+            }
+            else if (data > current.Get_Data())
+            {
+                pos = 2 * pos + 2;
+                current = current.Get_Right();
+            }
+            else if (data == current.Get_Data())
+            {
+                pos = 0;
+                return pos;
+            }
+        }
 
+        return pos;
+    }
+
+    private void Update_Visual()
+    {
+        int position = 0;
+        int parent_position = -1;
+        BinaryTreeNode current = head;
+        Queue<BinaryTreeNode> queue = new Queue<BinaryTreeNode>();
+        Queue<int> position_queue = new Queue<int>();
+        Queue<int> parent_queue = new Queue<int>();
+
+        queue.Enqueue(head);
+        position_queue.Enqueue(position);
+        parent_queue.Enqueue(parent_position);
+
+        while (queue.Count != 0)
+        {
+            current = queue.Dequeue();
+            position = position_queue.Dequeue();
+            parent_position = parent_queue.Dequeue();
+
+
+            current.Get_GameObject().transform.localPosition = positions[position];
+
+            if (position >= 15)
+            {
+                current.Get_GameObject().transform.Set_Child_Active(active: false, 2);
+                current.Get_GameObject().transform.Set_Child_Active(active: false, 3);
+            }
+            else
+            {
+                current.Get_GameObject().transform.Set_Child_Active(active: true, 2);
+                current.Get_GameObject().transform.Set_Child_Active(active: true, 3);
+            }
+
+            if (current.left == null)
+            {
+                current.Get_GameObject().transform.Get_Child(2).localScale = Vector3.one;
+                current.Get_GameObject().transform.Get_Child(2).eulerAngles = new Vector3(0,0,70);
+            }
+
+            if(current.right == null)
+            {
+                current.Get_GameObject().transform.Get_Child(3).localScale = Vector3.one;
+                current.Get_GameObject().transform.Get_Child(3).eulerAngles = new Vector3(0,0,100);
+
+            }
+
+            if (parent_position != -1)
+            {
+                if (current.Get_Child_Type() == BinaryTreeNode.Child_Type.Left)
+                {
+                    current.Get_Parent().Get_GameObject().transform.Get_Child(2).localScale = scales[parent_position];
+                    current.Get_Parent().Get_GameObject().transform.Get_Child(2).eulerAngles = new Vector3(0, 0, rotations_left[parent_position]);
+                }
+                else
+                {
+                    current.Get_Parent().Get_GameObject().transform.Get_Child(3).localScale = scales[parent_position];
+                    current.Get_Parent().Get_GameObject().transform.Get_Child(3).eulerAngles = new Vector3(0, 0, rotations_right[parent_position]);
+                }
+            }
+
+            
+
+            if (current.Get_Left() != null)
+            {
+                queue.Enqueue(current.Get_Left());
+                parent_queue.Enqueue(position);
+                position_queue.Enqueue(2 * position + 1);
+            }
+            if (current.Get_Right() != null)
+            {
+                queue.Enqueue(current.Get_Right());
+                parent_queue.Enqueue(position);
+                position_queue.Enqueue(2 * position + 2);
+            }
+        }
+
+    }
 
     public IEnumerator delete(long data)
     {
@@ -205,7 +236,7 @@ public class AVLTree : BinaryTree
 
             if (!current_parent.Is_Balanced())
             {
-                print("Rotate at " + current_parent.Get_Data());
+
 
                 v = current_parent;
                 w = Rebalance_Son(v);
