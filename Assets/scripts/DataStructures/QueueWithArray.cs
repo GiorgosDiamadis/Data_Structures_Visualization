b118fc5af793791c19b79ca33ee770d6ce0f5387
GiorgosDiamadis <56475498+GiorgosDiamadis@users.noreply.github.com>
2020-10-19 13:34:44 +0300

    q

--- Assets/scripts/DataStructures/QueueWithArray.cs
+++ Assets/scripts/DataStructures/QueueWithArray.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QueueWithArray : IDataStructure
 {
+    private int next_empty = -1;
+
     public override void Init()
     {
-        print(view);
+        view.transform.Destroy_All_Children();
+        init_number = 10;
+
+        for (int i = 0; i < init_number; i++)
+        {
+            if (i < 3)
+                create_cell(true);
+            else
+                create_cell();
+        }
+
+        next_empty = 3;
+        max_nodes = 10;
+
+        ViewHandler.Instance.Change_Grid(GridLayoutGroup.Axis.Vertical, GridLayoutGroup.Constraint.FixedRowCount, 1, new Vector2(30f, 30f));
     }
 
 }
